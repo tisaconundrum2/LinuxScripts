ae13b0c441113e82136a9300ffda82b9bcd61c77
Nicholas Finch <ngf4@nau.edu>
2018-07-17 14:50:33 -0700

    Success, script works don't do &&

--- VisudoAutomateWithSSH.sh
+++ VisudoAutomateWithSSH.sh
@@ -15,17 +15,17 @@ echo "Example: CIDSE-adoipe1_Lab_Admins"
 read -r Group
 Group="%FULTON\\\\\\$Group	ALL=(ALL:ALL) ALL"
 # add to the sudo file
-echo "$pwd" | sudo -Sv && cat /etc/sudoers > /tmp/sudoers.tmp
-echo "$pwd" | sudo -Sv && echo "$Group" >> sudo tee -a /tmp/sudoers.tmp
+echo $pwd | sudo -S cat /etc/sudoers > /tmp/sudoers.tmp
+echo $pwd | sudo -S echo "$Group" >> /tmp/sudoers.tmp
 clear
 echo "Output of sudoers file"
 echo
-echo "$pwd" | sudo -Sv && bash -s cat /tmp/sudoers.tmp
+echo $pwd | sudo -S cat /tmp/sudoers.tmp
 echo
 read -rp "Do the contents of the file look correct? [y/N] " answer
 
 if [[ $answer = [yY] ]]; then
-    echo "$pwd" | sudo -Sv && cp /tmp/sudoers.tmp /etc/sudoers
+    echo $pwd | sudo -S cp /tmp/sudoers.tmp /etc/sudoers
 else
     echo "Not commiting changes. Now exiting"
 fi
