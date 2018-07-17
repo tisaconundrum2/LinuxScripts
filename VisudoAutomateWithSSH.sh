fd1295a4f3b525b61502a4b6e8fb89eb21f0f33b
Nicholas Finch <ngf4@nau.edu>
2018-07-17 10:20:37 -0700

    escaping proper characters

--- VisudoAutomateWithSSH.sh
+++ VisudoAutomateWithSSH.sh
@@ -7,15 +7,13 @@ HOST='techs@'$HOST'.cidse.dhcp.asu.edu'
 #### Connect to the System ####
 echo 'Enter Techs password When Prompted'
 ssh -t "$HOST" '
-
-
 #### Run Commands ####
 echo "Please enter groupname without prefix"
-echo "The standad lab admin groupname is: "CIDSE-<professor ASUAD>_Lab_Admins""
+echo "The standad lab admin groupname is: \"CIDSE-<professor ASUAD>_Lab_Admins\""
 echo "Example: CIDSE-adoipe1_Lab_Admins"
 read -r Group
 Group="%FULTON\\\\\\$Group	ALL=(ALL:ALL) ALL"
-CidseItGroup=""%FULTON\\\cidse-it	ALL=(ALL:ALL) ALL"
+CidseItGroup='\''%FULTON\\\cidse-it	ALL=(ALL:ALL) ALL'\''
 # add to the sudo file
 cat /etc/sudoers > /etc/sudoers.tmp
 echo "$Group" >> -a /etc/sudoers.tmp
@@ -30,7 +28,7 @@ read -r answer
 
 if [[ $answer = [yY] ]]; then
     cp /etc/sudoers.tmp /etc/sudoers
-else;
+else
     echo "Not commiting changes. Now exiting"
 fi
 rm /etc/sudoers.tmp
